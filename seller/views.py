51e9cb1c43f8201b6a7bccd8bd3668ff5ab3639f
nayanajinson <nayanajinson28@gmail.com>
2025-11-18 14:25:42 +0530

    before merge

--- seller/views.py
+++ seller/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from os import remove
 
-# Create your views here.
+from django.shortcuts import render,redirect
+
+from seller.models import Product
+
+
+def view_product(request):
+    data=Product.objects.all()
+    return render(request,'seller/seller_dashboard.html')
+
+#
+# def delete_product(request,id):
+#     data=Product.objects.get(id=id)
+#     data.delete()
+#     return redirect('/')
+
+
+# def update_product(request,id):
+#     data=Product.objects.get(id=id)
+#     if request.method == "POST":
+#         seller = request.POST.get('seller')
+#         age = request.POST.get('age')
+#         data.seller = seller
+#         data.age = age
+#         data.save()
+#         return redirect('/')
+#     return render(request,'seller/seller_product.html')
