d76a9d3effadd1376cda191b2e785aa20669716e
nayanajinson <nayanajinson28@gmail.com>
2025-11-26 16:23:06 +0530

    * set

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="your-default-key")
 
 DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = 'core.User'
 
