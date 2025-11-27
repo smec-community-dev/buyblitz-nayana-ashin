2111e7b87d533c3ff6a9f92d61f5a962972630f4
nayanajinson <nayanajinson28@gmail.com>
2025-11-27 16:30:02 +0530

    full code

--- project/settings.py
+++ project/settings.py
@@ -93,10 +93,18 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # ============================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
+
 
 # ============================
 # PASSWORD VALIDATION
